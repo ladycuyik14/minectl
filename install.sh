4e30783d46d9382aa944b5332b4ee4c0ab2a514f
Richard Neumann <mail@richard-neumann.de>
2013-07-22 14:46:01 +0200

    Modified installation script for new user

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Minectl installation script
 #
 
+MC_USER=""$MC_USER""
+MC_HOME="/srv/$MC_USER"
+
 # Test if we are root
 if [ $EUID -ne 0 ]; then
 	echo "You need to be root to run `basename $0`" 1>&2
@@ -11,19 +14,19 @@ fi
 
 ROOT_DIR="$(dirname $(realpath $0))"
 
-# Add minectl user
+# Add "$MC_USER" user
 add_user() {
-	# Create new minectl user
-	if [ `grep -c ^"minecraft:" /etc/passwd` == "0" ]; then
-		/usr/sbin/useradd -mUrd /srv/minecraft minecraft
+	# Create new "$MC_USER" user
+	if [ `grep -c ^""$MC_USER":" /etc/passwd` == "0" ]; then
+		/usr/sbin/useradd -mUrd "$MC_HOME" "$MC_USER"
 	else
-		/usr/sbin/usermod -md /srv/minecraft minecraft
+		/usr/sbin/usermod -md "$MC_HOME" "$MC_USER"
 	fi
 }
 
-# Remove minectl user
+# Remove "$MC_USER" user
 remove_user() {
-	userdel $@ minecraft
+	userdel $@ "$MC_USER"
 }
 
 # Install necessary files
@@ -38,33 +41,34 @@ install_files() {
 	install -o root -g root -m 775 -t /usr/local/bin bin/*
 	install -o root -g root -m 775 -d /usr/local/libexec/minectl
 	install -o root -g root -m 775 -t /usr/local/libexec/minectl libexec/*
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/lang
-	install -o minectl -g minectl -m 775 -t /srv/minecraft/lang lang/*
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/jar
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/jar/repo
-	install -o minectl -g minectl -m 775 -t /srv/minecraft/jar/repo jar/repo/.[^.]*
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/backup
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/servers
-	install -o minectl -g minectl -m 775 -t /srv/minecraft/servers servers/.[^.]*
-	install -o minectl -g minectl -m 775 -d /srv/minecraft/event-handlers
-	install -o minectl -g minectl -m 775 -t /srv/minecraft/event-handlers event-handlers/*
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/lang
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/lang lang/*
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/jar
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/jar/repo
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/jar/repo jar/repo/.[^.]*
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/.backup
+	ln -s "$MC_HOME"/.backup "$MC_HOME"/backup
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/servers
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/servers servers/.[^.]*
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/event-handlers
+	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/event-handlers event-handlers/*
 
 	# Install system services
 	if [ -d /lib/systemd/system ]; then
-		install -o root -g root -m 775 -t /lib/systemd/system/ service/"minecraft@.service"
-		echo "Systemd service template '/lib/systemd/system/minecraft@.service' installed"
+		install -o root -g root -m 775 -t /lib/systemd/system/ service/""$MC_USER"@.service"
+		echo "Systemd service template '/lib/systemd/system/"$MC_USER"@.service' installed"
 	else
-		install -o root -g root -m 775 -t /etc/init.d/ service/minecraft
-		echo "Init service '/etc/init.d/minecraft' installed"
+		install -o root -g root -m 775 -t /etc/init.d/ service/"$MC_USER"
+		echo "Init service '/etc/init.d/"$MC_USER"' installed"
 	fi
 
 	# Set EN_us as default language
-	cd /srv/minecraft/lang
+	cd "$MC_HOME"/lang
 	ln -sf EN_us.lang default.lang
 
 	# Add binaries' path to the users's PATH variable
-	if [ -z "`grep "PATH=.*/usr/local/bin" /srv/minecraft/.bashrc`" ]; then
-		echo 'export PATH="$PATH:/usr/local/bin"' >> /srv/minecraft/.bashrc
+	if [ -z "`grep "PATH=.*/usr/local/bin" "$MC_HOME"/.bashrc`" ]; then
+		echo 'export PATH="$PATH:/usr/local/bin"' >> "$MC_HOME"/.bashrc
 	fi
 }
 
@@ -72,8 +76,8 @@ install_files() {
 uninstall_files() {
 	rm -f /usr/local/bin/{mcpasswd,mcsrv,minectl}
 	rm -Rf /usr/local/libexec/minectl
-	rm -f /etc/init.d/minecraft
-	rm -f "/lib/systemd/system/minecraft@.service"
+	rm -f /etc/init.d/"$MC_USER"
+	rm -f "/lib/systemd/system/"$MC_USER"@.service"
 	rm -f "/lib/systemd/system/minemon@.service"
 	rm -f /usr/local/share/man/man1/minectl.1
 }
@@ -83,13 +87,13 @@ disable_services() {
 	# Uninstall system services
 	if [ -d /lib/systemd/system ]; then
 		cd /etc/systemd/system/multi-user.target.wants/
-		for SERVICE in minecraft@*; do
+		for SERVICE in "$MC_USER"@*; do
 			systemctl disable $SERVICE
 			systemctl stop $SERVICE
 		done
 		echo "Systemd services uninstalled"
 	else
-		chkconfig minecraft off
+		chkconfig "$MC_USER" off
 		rm -f /etc/minectl.servers
 		rm -f /tmp/minectl.servers
 		echo "Init service uninstalled"
