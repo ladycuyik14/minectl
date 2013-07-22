fec190c43afc9d47cd1748ffa35aa508be53e07d
Richard Neumann <mail@richard-neumann.de>
2013-07-22 15:01:49 +0200

    Fixed some issues in install.sh and minectl

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ install_files() {
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/jar/repo
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/jar/repo jar/repo/.[^.]*
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/.backup
-	ln -s "$MC_HOME"/.backup "$MC_HOME"/backup
+	ln -s "$MC_HOME/.backup" "$MC_HOME/backup"
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/servers
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/servers servers/.[^.]*
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/event-handlers
