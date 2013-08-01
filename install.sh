ccf06b3f5a93bea8633492d08d894f7b99b7ea26
Richard Neumann <mail@richard-neumann.de>
2013-08-01 23:30:46 +0200

    Made minectl modular. Highly experimental

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #
 
 MC_USER="minecraft"
-MC_HOME="/srv/$MC_USER"
+MC_HOME="/home/$MC_USER"
 
 # Test if we are root
 if [ $EUID -ne 0 ]; then
@@ -40,7 +40,9 @@ install_files() {
 	install -o root -g root -m 775 -t /usr/local/share/man/man1 ../man/minectl.1
 	install -o root -g root -m 775 -t /usr/local/bin bin/*
 	install -o root -g root -m 775 -d /usr/local/libexec/"$MC_USER"
+	install -o root -g root -m 775 -d /usr/local/libexec/"$MC_USER"/plugins
 	install -o root -g root -m 775 -t /usr/local/libexec/"$MC_USER" libexec/*
+	install -o root -g root -m 775 -t /usr/local/libexec/"$MC_USER"/plugins libexec/plugins/*
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/lang
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -t "$MC_HOME"/lang lang/*
 	install -o "$MC_USER" -g "$MC_USER" -m 775 -d "$MC_HOME"/jar
